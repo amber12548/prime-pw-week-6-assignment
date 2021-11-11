7ea0ea3c280f714e1fe33919e0171f486a195270
amber12548 <amber12548@yahoo.com>
2021-11-10 18:39:46 -0600

    updated files

---
+++ assignment/client.js
@@ -0,0 +1,9 @@
+let garage = [];
+$ ( document ).ready ( addCar );
+
+
+
+/*
+Do not change newCar for base mode!
+HINT: You will need to gather the input values and then call this function, passing in those input values.
+*/
